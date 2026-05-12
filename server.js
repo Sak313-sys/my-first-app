ba4b9e5341b89af6728a2ff869c5d1a1c4e464de
jahid <zahid.abfl@gmail.com>
2026-05-13 00:19:50 +0600

    first app

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("My first live server!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
